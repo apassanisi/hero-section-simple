f6dfe48dd1cd31c1e327b9e31aa85d6012e0dcc8
Andrew Passanisi <andrew@specbooks.com>
2024-08-15 14:49:07 -0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,11 @@
     class="font-body w-screen h-screen flex flex-col items-center px-4 bg-gradient-to-b from-gray-50 to-[#d2d6db]"
   >
     <div
-      class="flex flex-col xl:flex-row h-full w-full shadow bg-white rounded-lg px-4 py-6 m-4 gap-12"
+      class="flex flex-col items-start lg:items-center justify-center lg:flex-row h-full w-full shadow bg-white rounded-lg px-4 lg:px-24 py-6 m-4 gap-12"
     >
-      <div class="flex flex-col gap-8 items-center justify-center">
+      <div class="flex flex-col gap-16 items-start justify-center">
         <div class="flex flex-col gap-4">
-          <h1 class="font-semibold text-4xl text-neutral-900 lg:text-5xl">
+          <h1 class="font-semibold text-4xl text-neutral-900 lg:text-6xl">
             Well crafted abstract images
           </h1>
           <span class="font-normal text-lg text-neutral-600 lg:text-xl"
@@ -17,19 +17,23 @@
         </div>
         <div class="flex flex-row gap-2 lg:gap-8">
           <button
-            class="bg-white px-6 rounded h-[48px] w-full lg:w-[213px]  lg:text-lg border-[0.5px] border-solid border-neutral-200"
+            class="w-[145px] flex justify-center items-center gap-2.5 grow bg-white px-6 py-4 rounded border-[0.5px] border-solid border-neutral-200"
           >
             Learn more
           </button>
           <button
-            class="bg-indigo-600 text-white px-6 rounded h-[48px] w-full lg:w-[213px] lg:text-lg  border-[0.5px] border-solid border-neutral-200"
+            class="w-[145px] flex justify-center items-center gap-2.5 grow bg-indigo-700 px-6 py-4 rounded text-white"
           >
             See pricing
           </button>
         </div>
       </div>
-      <div class="flex items-center justify-center">
-        <img src="./assets/hero-image.png" alt="Hero Image" class="w-full h-full max-h-[526px]" />
+      <div class="flex items-center justify-center max-h-[526px]">
+        <img
+          src="./assets/hero-image.png"
+          alt="Hero Image"
+          class="w-full h-full max-h-[526px]"
+        />
       </div>
     </div>
   </div>
